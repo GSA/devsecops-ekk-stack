9ce90af34f2fb665cfe88aba737873670a61e0ef
Jason Miller <jmiller@red-abstract.com>
2017-12-28 17:05:42 -0600

    Fixed outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,11 +1,7 @@
 output "es_domain_arn" {
-    value = "${aws_elasticsearch_domain.elasticsearch.es_domain_arn}"
+    value = "${aws_elasticsearch_domain.elasticsearch.arn}"
 }
 
-output "es_domain_url" {
-    value = "${aws_elasticsearch_domain.elasticsearch.es_domain_url}"
-}
-
-output "kinesis_firehose_delivery_name" {
-    value = "${blah3}"
+output "es_domain_endpoint" {
+    value = "${aws_elasticsearch_domain.elasticsearch.endpoint}"
 }
