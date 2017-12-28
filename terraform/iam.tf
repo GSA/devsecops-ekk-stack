d56213c88a43dc802110e8f26439e162b5561269
Jason Miller <jmiller@red-abstract.com>
2017-12-28 17:05:52 -0600

    Fixed IAM instance profile

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -43,6 +43,11 @@ resource "aws_iam_role" "elasticsearch_role" {
     assume_role_policy = "${data.template_file.ec2_assume_role_policy.rendered}"
 }
 
+resource "aws_iam_instance_profile" "elasticsearch_instance_profile" {
+  name  = "${var.elasticsearch_role_name}-instance-profile"
+  role = "${aws_iam_role.elasticsearch_role.name}"
+}
+
 resource "aws_iam_policy" "elasticsearch_policy" {
     name = "${var.elasticsearch_role_policy_name}"
     policy = <<EOF
