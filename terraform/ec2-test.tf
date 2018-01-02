66b67dcbcce664c83373ab39e9309ce39bbe8005
Jason Miller <jmiller@red-abstract.com>
2018-01-02 10:56:32 -0600

    Fix outbound traffic 🙄

--- terraform/ec2-test.tf
+++ terraform/ec2-test.tf
@@ -66,10 +66,16 @@ resource "aws_security_group" "main" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
-      from_port = 22
-      to_port = 22
+      from_port = 80
+      to_port = 80
       protocol = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
   }
 }
 
