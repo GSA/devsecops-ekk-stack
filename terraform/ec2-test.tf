3dd7aca8a8e9303a330d3e99ca97b0f10b8a9f05
Jason Miller <jmiller@red-abstract.com>
2018-01-02 14:25:19 -0600

    Properly indent ;)

--- terraform/ec2-test.tf
+++ terraform/ec2-test.tf
@@ -1,13 +1,13 @@
 module "test_vpc" {
   source = "github.com/terraform-aws-modules/terraform-aws-vpc"
 
-name = "ec2-test"
-cidr = "10.0.0.0/16"
-public_subnets = ["10.0.1.0/24"]
-enable_nat_gateway = "false"
-enable_dns_hostnames = "true"
-enable_dns_support = "true"
-azs = ["us-east-1c", "us-east-1d"]
+  name = "ec2-test"
+  cidr = "10.0.0.0/16"
+  public_subnets = ["10.0.1.0/24"]
+  enable_nat_gateway = "false"
+  enable_dns_hostnames = "true"
+  enable_dns_support = "true"
+  azs = ["us-east-1c", "us-east-1d"]
 }
 
 data "template_file" "user_data_template" {
