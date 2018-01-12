957151a70d65c67a5a9acc2ef513af295b5090dd
Jason Miller <jmiller@red-abstract.com>
2018-01-12 09:26:16 -0600

    Put back too many outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,10 +1,10 @@
-output "es_domain_arn" {
-    value = "${aws_elasticsearch_domain.elasticsearch.arn}"
-}
+# output "es_domain_arn" {
+#     value = "${aws_elasticsearch_domain.elasticsearch.arn}"
+# }
 
-output "es_domain_endpoint" {
-    value = "${aws_elasticsearch_domain.elasticsearch.endpoint}"
-}
+# output "es_domain_endpoint" {
+#     value = "${aws_elasticsearch_domain.elasticsearch.endpoint}"
+# }
 
 output "elasticsearch_instance_profile_id" {
     value = "${aws_iam_instance_profile.ekk_instance_profile.id}"
