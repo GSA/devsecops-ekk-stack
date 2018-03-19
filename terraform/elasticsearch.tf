97f21a1488ecd69e13e1ad421df1329c0a95f329
Jason Miller <jmiller@red-abstract.com>
2018-03-19 16:46:36 -0500

    Adding KMS/encryption support for Elasticsearch

--- terraform/elasticsearch.tf
+++ terraform/elasticsearch.tf
@@ -1,32 +1,37 @@
 resource "aws_elasticsearch_domain" "elasticsearch" {
-  domain_name = "${var.es_domain_name}"
+  domain_name           = "${var.es_domain_name}"
   elasticsearch_version = "${var.es_version}"
-  
+
   cluster_config {
-      dedicated_master_enabled = "${var.es_dedicated_master_enabled}"
-      instance_type = "${var.es_instance_type}"
-      instance_count = "${var.es_instance_count}"
-      zone_awareness_enabled = "${var.es_zone_awareness_enabled}"
-      dedicated_master_type = "${var.es_dedicated_master_instance_type}"
-      dedicated_master_count = "${var.es_dedicated_master_count}"
+    dedicated_master_enabled = "${var.es_dedicated_master_enabled}"
+    instance_type            = "${var.es_instance_type}"
+    instance_count           = "${var.es_instance_count}"
+    zone_awareness_enabled   = "${var.es_zone_awareness_enabled}"
+    dedicated_master_type    = "${var.es_dedicated_master_instance_type}"
+    dedicated_master_count   = "${var.es_dedicated_master_count}"
   }
 
   advanced_options {
-      "rest.action.multi.allow_explicit_index" = "${var.es_advanced_allow_explicit_index}"
+    "rest.action.multi.allow_explicit_index" = "${var.es_advanced_allow_explicit_index}"
   }
 
   ebs_options {
-      ebs_enabled = "${var.es_ebs_enabled}"
-      iops = "${var.es_ebs_iops}"
-      volume_size = "${var.es_ebs_volume_size}"
-      volume_type = "${var.es_ebs_volume_type}"
+    ebs_enabled = "${var.es_ebs_enabled}"
+    iops        = "${var.es_ebs_iops}"
+    volume_size = "${var.es_ebs_volume_size}"
+    volume_type = "${var.es_ebs_volume_type}"
+  }
+
+  encrypt_at_rest {
+    enabled    = "true"
+    kms_key_id = "${var.es_kms_key_id != "" ? var.es_kms_key_id : aws_kms_key.es_kms_key.key_id}"
   }
 
   snapshot_options {
-      automated_snapshot_start_hour = "${var.es_snapshot_start_hour}"
+    automated_snapshot_start_hour = "${var.es_snapshot_start_hour}"
   }
 
-    access_policies = <<CONFIG
+  access_policies = <<CONFIG
 {
     "Version": "2012-10-17",
     "Statement": [
--- terraform/kms.tf
+++ terraform/kms.tf
@@ -1,11 +1,17 @@
 resource "aws_kms_key" "s3_logging_kms_key" {
-    count = "${var.s3_kms_key_arn == "" ? 1 : 0}"
+  count = "${var.s3_kms_key_arn == "" ? 1 : 0}"
 
-    description = "S3 Logging KMS Key - Created by Terraform"
+  description = "S3 Logging KMS Key - Created by Terraform"
+}
+
+resource "aws_kms_key" "es_kms_key" {
+  count = "${var.es_kms_key_id == "" ? 1 : 0}"
+
+  description = "ES data KMS Key - Created by Terraform"
 }
 
 resource "aws_kms_key" "kinesis_stream_kms_key" {
-    count = "${var.ekk_kinesis_stream_kms_key_id == "" ? 1 : 0}"
+  count = "${var.ekk_kinesis_stream_kms_key_id == "" ? 1 : 0}"
 
-    description = "Kinesis Stream KMS Key - Created by Terraform"
+  description = "Kinesis Stream KMS Key - Created by Terraform"
 }
